bae830cfae0ce8e46d0357a39cf65c387026c586
Gh-Yeh <yehyaghady@gmail.com>
2023-10-17 22:43:39 -0700

    Tasktwo

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
