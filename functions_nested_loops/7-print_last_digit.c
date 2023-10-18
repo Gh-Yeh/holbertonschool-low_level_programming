79d3a4a27315e7c3487a131ea2285754167dcc7a
Gh-Yeh <yehyaghady@gmail.com>
2023-10-18 00:24:41 -0700

    RemaningTasks

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the argument passed
+ * Return: last digit of a number
+ */
+
+int print_last_digit(int a)
+{
+	int ld;
+
+	ld = a % 10;
+	if (ld < 0)
+	{
+		ld = ld * -1;
+	}
+	_putchar(ld + '0');
+	return (ld);
+}
