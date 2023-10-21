a6d64677a7e1a0a47f62a48a718a8c7ab9bbe6d1
Gh-Yeh <yehyaghady@gmail.com>
2023-10-21 16:35:31 -0700

    ADVtasks

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned long int x = 1231952;
+	unsigned long int i;
+	unsigned long int bigF;
+
+	for (i = 1; i < x; i++)
+	{
+		if (x % i == 0)
+		{
+			int j;
+			int count = 0;
+
+			for (j = 2; j < i; j++)
+			{
+				if (i % j == 0)
+					count++;
+				if (count > 0)
+					continue;
+			}
+			if (count == 0)
+			{
+				bigF = i;
+			}
+		}
+	}
+	printf("%lu", bigF);
+	return (0);
+
+}
