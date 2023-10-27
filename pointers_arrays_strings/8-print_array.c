148dbe3178b0240ba385cdcedd38429d1962daf3
Gh-Yeh <yehyaghady@gmail.com>
2023-10-27 15:15:09 -0700

    last fix

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,17 +12,13 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < (n - 1); i++)
 	{
-		if (i < n - 1)
-		{
-			printf("%d, ", *a);
-		}
-		else
-		{
-			printf("%d", *a);
-		}
-		a++;
+		printf("%d, ", a[i]);
 	}
-	_putchar('\n');
+	if (i == (n - 1))
+	{
+		printf("%d", a[n - 1]);
+	}
+	printf("\n");
 }
