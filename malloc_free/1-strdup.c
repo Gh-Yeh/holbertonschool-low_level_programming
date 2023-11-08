c24803cf98172f844c02ef7d7aacaf06c25f26bd
Gh-Yeh <yehyaghady@gmail.com>
2023-11-08 15:50:58 -0800

    tasks 1 and 2

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+{
+	int count = 0;
+	int i;
+
+	while (*(str + count))
+		count++;
+
+	char *ch = (char *)malloc((count + 1) * (sizeof(char)));
+
+	if (str == NULL || ch == NULL)
+		return (NULL);
+
+	for (i = 0; i <= count; i++)
+	{
+		ch[i] = str[i];
+	}
+
+	return (ch);
+}
---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+
+	int count = 0, count1 = 0;
+	int i, j;
+
+	while (*(s1 + count))
+		count++;
+
+	while (*(s2 + count1))
+		count1++;
+
+	char *ch = (char *)malloc(((count + count1) + 1) * (sizeof(char)));
+
+	if (s1 == NULL || s2 == NULL || ch == NULL)
+		return (NULL);
+
+	for (i = 0; i < count; i++)
+	{
+		ch[i] = s1[i];
+	}
+	for (i = count, j = 0; i <= (count + count1); i++, j++)
+	{
+		ch[i] = s2[j];
+	}
+	return (ch);
+}
