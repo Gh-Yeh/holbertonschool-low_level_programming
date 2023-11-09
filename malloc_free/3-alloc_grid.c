d9fb3eaf1bf0b0d80ad5f80eb8f396c134b88d34
Gh-Yeh <yehyaghady@gmail.com>
2023-11-08 20:04:39 -0800

    fixes

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -22,8 +22,14 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		ch[i] = (int *)malloc(width * sizeof(int));
+
 		if (ch[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(ch[i]);
+
 			return (NULL);
+		}
 	}
 	for (i = 0; i < height; i++)
 	{
